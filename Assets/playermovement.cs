95a8f1d60f0a4372ef7bfb4842f52837be6c04f5
Loihi77 <robsungpark@gmail.com>
2024-10-21 18:23:31 -0700

    messaging rn fr fr
    message?

---
+++ Assets/playermovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playermovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("MEOW!");
+    }
+
+    void OnMove(InputValue value)
+    {
+        Vector2 dir = value.Get<Vector2>();
+        rb.velocity = dir * 10;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
